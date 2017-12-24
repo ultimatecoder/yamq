623e9035ca3ebf1ae5213d7bf120b010f7d968ab
Jaysinh Shukla <jaysinhp@gmail.com>
2017-12-24 13:09:27 +0530

    [ADD] Adding test case to assert the license note at the beginning of each file.

--- test/functional/test_license.py
+++ test/functional/test_license.py
@@ -1,6 +1,27 @@
-import os
+# Copyright (C) 2017-2018 Jaysinh Shukla
+# Please find copy of license at "LICENSE"
+# at the root of the project.
+
+
+import glob
 import unittest
 
 
 class TestLicense(unittest.TestCase):
-    pass
+
+    def test_each_file_starts_with_license(self):
+        license = [
+            '# Copyright (C) 2017-2018 Jaysinh Shukla',
+            '# Please find copy of license at "LICENSE"',
+            '# at the root of the project.',
+        ]
+        files = glob.glob("**/*.py", recursive=True)
+        for file_ in files:
+            with open(file_) as file_obj:
+                heading = file_obj.read(114)
+            heading = heading.splitlines() if heading else []
+            message = (
+                "\nFile: {} is missing license note. It is compulsory to add a"
+                "license note to each file."
+            )
+            self.assertListEqual(heading, license, message.format(file_))
